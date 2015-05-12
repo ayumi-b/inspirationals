090cd3250ea6859087f79e505ed6c6e80d7df022
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-05-12 09:23:47 -0500

    created basic usage test page and refactored

--- test/integration/test_adding_new_quote.rb
+++ test/integration/test_adding_new_quote.rb
@@ -40,46 +40,5 @@ require_relative '../test_helper'
 
 class AddingNewQuoteTest < Minitest::Test
 
-  def test_gives_minimum_arguments
-    shell_output = ""
-    expected_output = ""
-    IO.popen('./inspirationals') do |pipe|
-      expected_output = "[Help] Run as: ./inspirationals manage"
-      shell_output = pipe.read
-    end
-    assert_equal expected_output, shell_output
-  end
-
-  def test_manage_argument_not_given
-    shell_output = ""
-    expected_output = ""
-    IO.popen('./inspirationals cats') do |pipe|
-      expected_output = "[Help] Run as: ./inspirationals manage"
-      shell_output = pipe.read
-    end
-    assert_equal expected_output, shell_output
-  end
-
-  def test_manage_correct_argument_then_exit
-    shell_output = ""
-    expected_output = ""
-    IO.popen('./inspirationals manage', 'r+') do |pipe|
-      expected_output =<<EOS
-1. Add a Dr. Phil quote
-2. Add a Tiger Mom quote
-3. List all Dr. Phil quotes
-4. List all Tiger Mom quotes
-5. Exit
-EOS
-      pipe.puts "5"
-      expected_output << "Tomorrow's just a future yesterday.\n"
-      pipe.close_write
-      shell_output = pipe.read
-    end
-    assert_equal expected_output, shell_output
-  end
-
-
-
 
 end

c6958c3cc82e368d0e8a9d2abe881a2461b6f74e
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-05-13 14:52:46 -0500

    controller tests currentlypassing

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,12 +1,24 @@
+require 'highline/import'
+
 class QuotesController
   def index
     if Quote.count > 0
       quotes = Quote.all
+      quotes_string = ""
       quotes.each_with_index do |quote, index|
-        say("#{index + 1}. #{quote.name}")
+        quotes_string << "#{index + 1}. #{quote.name}\n"
       end
+      quotes_string
     else
-      say("No quotes found. Add a quote.\n")
+      "there are no quotes found. Add a quote.\n"
+    end
+  end
+
+  def add(name)
+    name_cleaned = name.strip
+    unless /^\d+$/.match(name_cleaned)
+      Quote.create(name_cleaned)
+      name_cleaned
     end
   end
 end
--- test/controllers/test_quotes_controller.rb
+++ test/controllers/test_quotes_controller.rb
@@ -3,10 +3,32 @@ require_relative '../test_helper'
 describe QuotesController do
 
   describe ".index" do
-
+    let(:controller) {QuotesController.new}
+    
+    it "should say no quotes found when empty" do
+      actual_output = controller.index
+      expected_output = "there are no quotes found. Add a quote.\n"
+      assert_equal expected_output, actual_output
+    end
   end
 
   describe ".add" do
+    let(:controller) {QuotesController.new}
 
+    it "should add a quote to database" do
+      controller.add("feline power")
+      assert_equal 1, Quote.count
+    end
+
+    it "should not add quote all spaces" do
+      quote_name = "        "
+      assert_raises(ArgumentError){ controller.add(quote_name) }
+    end
+
+    it "should only add quotes that make sense" do
+      quote_name = "111111111111"
+      controller.add(quote_name)
+      assert_equal 0, Quote.count
+    end
   end
 end

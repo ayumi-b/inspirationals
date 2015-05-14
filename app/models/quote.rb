54f3e5dcb8f446ec32c0f91b1e04b7c280ef6e16
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-05-14 15:59:38 -0500

    added new tests, added new accessors/readers

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,10 @@
 class Quote
+  attr_reader :id, :errors
   attr_accessor :name
+
+  def initialize(name = nil)
+    self.name = name 
+  end
 
   def self.all
     Database.execute("select name from quotes order by name ASC").map do |row|
@@ -12,12 +17,29 @@ class Quote
   def self.count
     Database.execute("select count(id) from quotes")[0][0]
   end
-
-  def self.create(name)
-    if name.empty?
-      raise ArgumentError.new
+ 
+  def valid?
+    if name.nil? or name.empty? or /^\d+$/.match(name)
+      @errors = "\"#{name}\" is not a valid quote."
+      false
     else
-      Database.execute("INSERT INTO quotes (name) VALUES (?)", name)
+     @errors = nil
+     true
     end
   end
+
+  def save
+    return false unless valid?
+    Database.execute("INSERT INTO quotes (name) VALUES (?)", name)
+    @id = Database.execute("SELECT last_insert_rowid()")[0]['last_insert_rowid()']
+  end
+
+
+  #def self.create(name)
+    #return if /^\d+$/.match(name)
+    #if name.empty?
+      #raise ArgumentError.new
+    #else
+      #Database.execute("INSERT INTO quotes (name) VALUES (?)", name)
+    #end
 end
--- test/models/test_quote.rb
+++ test/models/test_quote.rb
@@ -18,7 +18,7 @@ describe Quote do
       end
       it "should return the quotes in alphabetical order" do
         expected = ["Apple", "Cat", "Dog"]
-        actual = Quote.all.map { |quote| quote.name }
+        actual = Quote.all.map{ |quote| quote.name }
         assert_equal expected, actual
       end
     end
@@ -42,18 +42,117 @@ describe Quote do
    end
   end
 
-  describe "#create" do
-    describe "if we need to add quotes" do
-      it "should add a quote" do
-        Quote.create("Best or none.")
-        assert_equal 1, Quote.count
-      end
+  #describe "#create" do
+    #describe "if we need to add quotes" do
+      #it "should add a quote" do
+        #Quote.create("Best or none.")
+        #assert_equal 1, Quote.count
+      #end
 
-      it "should reject empty strings" do
-        assert_raises(ArgumentError) { Quote.create("") }
-      end
+      #it "should reject empty strings" do
+        #assert_raises(ArgumentError) { Quote.create("") }
+      #end
+    #end
+  #end
+
+  describe ".initialize" do
+    it "sets the name attribute" do
+      quote = Quote.new("foo")
+      assert_equal "foo", quote.name
     end
   end
 
+  describe ".save" do
+    describe "if the model is valid" do
+      let(:quote){ Quote.new("Hello Kitty")}
+      it "should return true" do
+        assert quote.save
+      end
+      it "should save the model to the database" do
+        quote.save
+        last_row = Database.execute("SELECT * FROM quotes")[0]
+        database_name = last_row['name']
+        assert_equal "Hello Kitty", database_name
+      end
+      it "should populate the model with id from the database" do 
+        quote.save
+        last_row = Database.execute("SELECT * FROM quotes")[0]
+        database_id = last_row['id']
+        assert_equal database_id, quote.id
+      end
+   end
 
+   describe "if the model is invalid" do
+     let(:quote){Quote.new("")}
+     it "should return false" do 
+       refute quote.save
+     end
+     it "should not save the model to the database" do
+       quote.save
+       assert_equal 0, Quote.count
+     end
+     it "should populate the error messages" do
+       quote.save
+       assert_equal "\"\" is not a valid quote.", quote.errors
+     end
+    end
+  end
+
+   describe ".valid?" do
+     describe "with valid data" do
+       let(:quote){ Quote.new("kitties are great")}
+       it "returns true" do
+         assert quote.valid?
+       end
+       it "should set errors to nil" do
+         quote.valid?
+         assert quote.errors.nil?
+       end
+     end
+     describe "with no name" do
+       let(:quote){ Quote.new(nil)}
+       it "returns false" do
+         refute quote.valid?
+       end
+       it "sets the error message" do
+         quote.valid?
+       assert_equal "\"\" is not a valid quote.", quote.errors
+       end
+     end
+    describe "with empty name" do
+      let(:quote){ Quote.new("")}
+      it "returns false" do 
+        refute quote.valid?
+      end
+       it "sets the error message" do
+         quote.valid?
+       assert_equal "\"\" is not a valid quote.", quote.errors
+       end
+    end
+    describe "with a name with no letter characters" do
+      let(:quote){ Quote.new("777") }
+      it "returns false" do
+        refute quote.valid?
+      end
+      it "sets the error message" do
+        quote.valid?
+        assert_equal "\"777\" is not a valid quote.", quote.errors
+      end
+    end
+    describe "with a previously invalid name" do
+      let(:quote){ Quote.new("666") }
+      before do
+        refute quote.valid?
+        quote.name = "Eat a pop tart"
+        assert_equal "Eat a pop tart", quote.name
+      end
+      it "should return true" do
+        assert quote.valid?
+      end
+      it "should not have an error message" do
+        quote.valid?
+        assert_nil quote.errors
+      end
+    end
+  end
 end

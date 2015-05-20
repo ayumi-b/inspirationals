cc581a85424418e58942b897f986d79bb2968c13
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-05-20 13:20:24 -0500

    quotes controller with better functionality

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,17 +1,40 @@
 require "highline/import"
 
 class QuotesController
-  def index
+
+
+  def index_phil
     if Quote.count > 0
       quotes = Database.execute("SELECT name FROM quotes WHERE author_id==1")
-      #dr_quotes = quotes[0]['name']
       choose do |menu|
-        menu.prompt = ""
+        menu.header = "Please pick which Dr. Phil inspirational you'd like to access.\n"
         quotes.each do |dr_quotes|
           quotes = dr_quotes['name']
           menu.choice(quotes){ action_menu(quotes)}
         end
-        menu.choice("Exit")
+        menu.choice("Exit. Please let me out.") do
+          say("Have a good day.")
+          exit
+        end
+      end
+    else
+      say("there are no quotes found. Add a quote.\n")
+    end
+  end
+
+  def index_tiger
+    if Quote.count > 0
+      quotes = Database.execute("SELECT name FROM quotes WHERE author_id==2")
+      choose do |menu|
+        menu.header = "Please pick which Tiger Mom inspirational you'd like to access.\n"
+        quotes.each do |dr_quotes|
+          quotes = dr_quotes['name']
+          menu.choice(quotes){ action_menu(quotes)}
+        end
+        menu.choice("Exit. Please Let me out.") do
+          say("Have a good day.")
+          exit
+        end
       end
     else
       say("there are no quotes found. Add a quote.\n")
@@ -21,7 +44,6 @@ class QuotesController
   def action_menu(quote)
     say("Would you like to?")
     choose do |menu|
-      menu.prompt = ""
       menu.choice("Edit") do
         edit(quote)
       end
@@ -34,11 +56,33 @@ class QuotesController
     end
   end
 
-  def add(name)
+  def add_tiger
+    loop do
+          user_input = ask("Please type in your Tiger Mom quote.")
+          response = quotes_controller.add(user_input)
+          say(response) unless response.nil?
+          if /has\sbeen\sadded.$/.match(response)
+            break
+          end
+        end
+  end
+
+  def add_phil
+        loop do
+          user_input = ask("Please type in your Dr. Phil quote.")
+          response = quotes_controller.add(user_input)
+          say(response) unless response.nil?
+          if /has\sbeen\sadded.$/.match(response)
+            break
+          end
+        end
+  end
+
+  def add(name, author_id, quote_type_id )
     name_cleaned = name.strip
     quote = Quote.new(name_cleaned)
     if quote.save
-      "\"#{name}\" has been added.\n"
+      "\"#{quote}\" has been added.\n"
     else
       quote.errors
     end
@@ -51,7 +95,7 @@ class QuotesController
         say("Quote has been updated to: \"#{scenario.name}\"")
         return
       else
-        say(quote.errors)  
+        say(quote.errors) 
       end
     end
   end
